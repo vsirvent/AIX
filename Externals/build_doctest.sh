6f77ae2375511e6ce0bb18f91fca6384e6165daa
Arkin <arkin.terli@gmail.com>
2024-05-10 18:40:35 -0700

    Fix logical core count in doctest build file.

--- Externals/build_doctest.sh
+++ Externals/build_doctest.sh
@@ -7,6 +7,7 @@
 lib_name=doctest
 lib_version=2.4.11
 lib_url=https://github.com/doctest/doctest.git
+logical_core_count=$(nproc 2>/dev/null || sysctl -n hw.logicalcpu)
 
 pushd .
 
@@ -20,7 +21,7 @@ mkdir build
 cd build
 
 cmake .. -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=FALSE -DCMAKE_INSTALL_PREFIX=../installed
-cmake --build . --target install -- -j `nproc --all`
+cmake --build . --target install -- -j $logical_core_count
 
 cd ..
 rm -rf .git .gitattributes .github .gitignore build
