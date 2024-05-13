5f9300b048c9b453df13144e9c8bef2e24c3b636
Arkin <arkin.terli@gmail.com>
2024-05-13 12:20:22 -0700

    Remove redundant grep options export in build.sh file.

--- build.sh
+++ build.sh
@@ -4,9 +4,6 @@
 #  Copyright (c) 2024-Present, Arkin Terli. All rights reserved.
 #
 
-# The following line requires for macOS, otherwise configure application fails to find grep.
-export GREP_OPTIONS="--color=auto"
-
 function showHelp()
 {
     echo ""
