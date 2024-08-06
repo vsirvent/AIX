385c4ee278a6249cfe8c37180f9c19a34dfd3f07
Arkin <arkin.terli@gmail.com>
2024-08-06 02:17:58 -0700

    Increase the epsilon value for CheckVectorApproxValue() method to increase tolerance in Approx() use cases.

--- Targets/AIXTests/Utils.hpp
+++ Targets/AIXTests/Utils.hpp
@@ -15,12 +15,12 @@
 #include <doctest/doctest.h>
 // System includes
 
-inline auto Approx(auto value, double epsilon = 1e-5)
+inline auto Approx(auto value, double epsilon = 1e-4)
 {
     return doctest::Approx(value).epsilon(epsilon);
 }
 
-inline void CheckVectorApproxValues(const aix::TensorValue & results, const aix::TensorValue & expected)
+inline void CheckVectorApproxValues(const aix::TensorValue & results, const aix::TensorValue & expected, double epsilon = 1e-4)
 {
     if (results.dataType() != expected.dataType())
     {
@@ -36,68 +36,68 @@ inline void CheckVectorApproxValues(const aix::TensorValue & results, const aix:
     {
         for (size_t i=0; i<expected.size(); ++i)
         {
-            CHECK(results.data<double>()[i] == Approx(expected.data<double>()[i]));
+            CHECK(results.data<double>()[i] == Approx(expected.data<double>()[i], epsilon));
         }
     }
     else if (results.dataType() == aix::DataType::kFloat32)
     {
         for (size_t i=0; i<expected.size(); ++i)
         {
-            CHECK(results.data<float>()[i] == Approx(expected.data<float>()[i]));
+            CHECK(results.data<float>()[i] == Approx(expected.data<float>()[i], epsilon));
         }
     }
     else if (results.dataType() == aix::DataType::kFloat16)
     {
         for (size_t i=0; i<expected.size(); ++i)
         {
-            CHECK(results.data<aix::float16_t>()[i] == Approx(expected.data<aix::float16_t>()[i]));
+            CHECK(results.data<aix::float16_t>()[i] == Approx(expected.data<aix::float16_t>()[i], epsilon));
         }
     }
     else if (results.dataType() == aix::DataType::kBFloat16)
     {
         for (size_t i=0; i<expected.size(); ++i)
         {
-            CHECK(results.data<aix::bfloat16_t>()[i] == Approx(expected.data<aix::bfloat16_t>()[i]));
+            CHECK(results.data<aix::bfloat16_t>()[i] == Approx(expected.data<aix::bfloat16_t>()[i], epsilon));
         }
     }
     else if (results.dataType() == aix::DataType::kInt64)
     {
         for (size_t i=0; i<expected.size(); ++i)
         {
-            CHECK(results.data<int64_t>()[i] == Approx(expected.data<int64_t>()[i]));
+            CHECK(results.data<int64_t>()[i] == Approx(expected.data<int64_t>()[i], epsilon));
         }
     }
     else if (results.dataType() == aix::DataType::kInt32)
     {
         for (size_t i=0; i<expected.size(); ++i)
         {
-            CHECK(results.data<int32_t>()[i] == Approx(expected.data<int32_t>()[i]));
+            CHECK(results.data<int32_t>()[i] == Approx(expected.data<int32_t>()[i], epsilon));
         }
     }
     else if (results.dataType() == aix::DataType::kInt16)
     {
         for (size_t i=0; i<expected.size(); ++i)
         {
-            CHECK(results.data<int16_t>()[i] == Approx(expected.data<int16_t>()[i]));
+            CHECK(results.data<int16_t>()[i] == Approx(expected.data<int16_t>()[i], epsilon));
         }
     }
     else if (results.dataType() == aix::DataType::kInt8)
     {
         for (size_t i=0; i<expected.size(); ++i)
         {
-            CHECK(results.data<int8_t>()[i] == Approx(expected.data<int8_t>()[i]));
+            CHECK(results.data<int8_t>()[i] == Approx(expected.data<int8_t>()[i], epsilon));
         }
     }
     else if (results.dataType() == aix::DataType::kUInt8)
     {
         for (size_t i=0; i<expected.size(); ++i)
         {
-            CHECK(results.data<uint8_t>()[i] == Approx(expected.data<uint8_t>()[i]));
+            CHECK(results.data<uint8_t>()[i] == Approx(expected.data<uint8_t>()[i], epsilon));
         }
     }
 }
 
-inline void CheckVectorApproxValues(const aix::Tensor & results, const aix::Tensor & expected)
+inline void CheckVectorApproxValues(const aix::Tensor & results, const aix::Tensor & expected, double epsilon = 1e-4)
 {
-    CheckVectorApproxValues(results.value(), expected.value());
+    CheckVectorApproxValues(results.value(), expected.value(), epsilon);
 }
